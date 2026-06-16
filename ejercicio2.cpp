64a0ef3e2df53f481f8b9d3e6afb9be9818d0e45
ninaparadzhanian <fannysunset24@gmail.com>
2026-06-16 01:11:46 -0600

    On branch ejercicio1 Changes to be committed:

---
+++ ejercicio2.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+main(void){
+    double base = 11.5, altura= 3.0, area=0.0;
+    area=(base*altura)/2;
+    printf("Area= %g\n", area);
+    return 0;
+    system("pause");
+}
